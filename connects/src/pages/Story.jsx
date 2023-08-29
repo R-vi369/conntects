ab2795593903cf4e9d930a5032557823a0e60499
ravi <sama369.rk@gmail.com>
2023-08-29 16:04:36 +0530

    story completed

--- connects/src/pages/Story.jsx
+++ connects/src/pages/Story.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import '../styles/Story.scss'
 import storyApi from '../story.json'
+
 const Story = () => {
   return (
     <>
-    <div className="story">
-      <div className="story_container">
-        
-          
- {
-    storyApi.map(items =>{
-      return(
-        <div className="box" key={items.id}>
-          <img src= {items.img} alt=""/>
-          <p>
-          {items.Name}
-          </p>
-        </div>
-      )
+      <div className="story">
+        <div className="story_container">
 
-    })
- }
+
+          {
+            storyApi.map(items => {
+
+              return (
+                <div className="box" key={items.id}>
+                  <img src={items.img} alt="" />
+                  <p>
+                    {items.Name}
+                  </p>
+                </div>
+              )
+
+            })
+          }
+        </div>
       </div>
-    </div>
-    
+
     </>
   )
 }

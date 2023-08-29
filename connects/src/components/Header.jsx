1642e5e1ac6a3440308d3fad6d8dacfe2e536e63
ravi <sama369.rk@gmail.com>
2023-08-29 16:50:59 +0530

    making responsive

--- connects/src/components/Header.jsx
+++ connects/src/components/Header.jsx
@@ -1,17 +1,20 @@
 import '../styles/header.scss'
-import { LiaHomeSolid , LiaShoppingBasketSolid } from 'react-icons/lia'
+import { LiaHomeSolid, LiaShoppingBasketSolid } from 'react-icons/lia'
 import { GiMagicLamp } from 'react-icons/gi'
-import {TbFriends } from 'react-icons/tb'
-import {HiOutlineChatAlt} from 'react-icons/hi'
-import {Link} from 'react-router-dom'
+import { TbFriends } from 'react-icons/tb'
+import { HiOutlineChatAlt } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <>
             <div className="header">
                 <div className="header_logo">
                     <Link to='/' className='LinkMain'>
-                    Connects
-                    <GiMagicLamp  className='header_icons'/>
+                        <div className="connects">
+
+                            Connects
+                        </div>
+                        <GiMagicLamp className='header_icons' />
                     </Link>
                 </div>
 
@@ -19,35 +22,35 @@ const Header = () => {
 
                 <div className="header_icons_text">
                     <Link to='/' className="header_home">
-                    <LiaHomeSolid className='header_icon active_icon' />
-                    <p>Home</p>
+                        <LiaHomeSolid className='header_icon active_icon' />
+                        <p>Home</p>
                     </Link>
 
                     <Link to='/Find' className="header_friends">
 
-                    <TbFriends  className='header_icon'/>
-                    <p>Find</p>
+                        <TbFriends className='header_icon' />
+                        <p>Find</p>
                     </Link>
 
                     <Link to='/Message' className="header_messaging">
 
-                    <HiOutlineChatAlt className='header_icon'/>
-                    <p>Messaging</p>
+                        <HiOutlineChatAlt className='header_icon' />
+                        <p>Messaging</p>
                     </Link>
 
                     <Link to='/Story' className="header_shop">
 
-                    <LiaShoppingBasketSolid className='header_icon'/>
-                    <p>Story</p>
+                        <LiaShoppingBasketSolid className='header_icon' />
+                        <p>Story</p>
                     </Link>
 
                 </div>
 
 
-                   <div className="avatar">
+                <div className="avatar">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRcLUO1YT9xjfHdUJXTr3b_vl9bd2R0FIbIA&usqp=CAU" alt="" />
-                      <h3>Ravi</h3>
-                   </div>
+                    <h3>Ravi</h3>
+                </div>
 
             </div>
 

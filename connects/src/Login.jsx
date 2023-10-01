ef16e4b2dc7891d1388299db47c2f5f5c7f32ef8
ravi <sama369.rk@gmail.com>
2023-10-01 09:31:12 +0530

    login

---
+++ connects/src/Login.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // You can add your authentication logic here
+    // For a basic example, you can just console.log the email and password
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-page">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
